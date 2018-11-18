21f2c8e8492d37722efe8303a54cccb9f96f7e3d
ArtMalinowski <Artur.Malinowski95@gmail.com>
2018-11-18 19:22:44 +0000

    Fixed Typos

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 
-public class cameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour {
 
     // Use this for initialization
     public Transform target;
 
     public float speedOFCamer = 0.5f;
-    public Vector3 offset;
+    public Vector3 distanceToCamera;
     public float rotation;
 
     private void Start()
@@ -16,8 +16,8 @@ public class cameraFollow : MonoBehaviour {
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(target.position, desiredPosition, speedOFCamer);
-        transform.position = smoothedPosition;
+        Vector3 newPosition = target.position + distanceToCamera;
+        Vector3 smoothedChangePositionForCamera = Vector3.Lerp(target.position, newPosition, speedOFCamer);
+        transform.position = smoothedChangePositionForCamera;
     }
 }
--- Assets/Scripts/endlessMountain.cs
+++ Assets/Scripts/endlessMountain.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class endlessMountain : MonoBehaviour {
+public class EndlessMountain : MonoBehaviour {
 
 
 
