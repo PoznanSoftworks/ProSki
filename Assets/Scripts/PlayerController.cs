bb6b98dadc0e858a4e3713133c29b8e078b68656
anj42 <anj42@aber.ac.uk>
2018-11-18 22:55:31 +0000

    poprawianie sterowania w toku

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerController : MonoBehaviour {
 
 
     private ConstantForce cf;
     private Rigidbody rb;
+
+    private DateTime checkTimeStart;
+    private DateTime checkTimeEnd;
+    private double tapController;
 
     void Start()
     {
@@ -14,22 +19,46 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Update()
-    {
-
-
-
+    { 
+        
         if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            
-            print("left");
-            rb.AddRelativeForce(Vector3.left * 1);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            print("right");
+        { 
+            checkTimeStart = DateTime.Now;
             rb.AddRelativeForce(Vector3.right * 1);
         }
 
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            tapController = DateTime.Now.Subtract(checkTimeStart).TotalSeconds;
+            //print(tapController);
+
+            if (tapController < 0.005)
+            {
+                rb.AddRelativeForce(Vector3.right * 50);
+                checkTimeStart = DateTime.Now;
+                tapController = 0;
+                print(tapController);
+            } 
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            checkTimeStart = DateTime.Now;
+            rb.AddRelativeForce(Vector3.left * 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            tapController = DateTime.Now.Subtract(checkTimeStart).TotalSeconds;
+            //print(tapController);
+            if (tapController < 0.005)
+            {
+                print(tapController);
+                rb.AddRelativeForce(Vector3.left * 50);
+                checkTimeStart = DateTime.Now;
+                tapController = 0;
+            }
+        }
 
     }
 }
